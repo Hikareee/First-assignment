3e81e9d70959d943eb8378fa3086b5204b413bdd
HikariNoRyu <88818748+HikariNoRyu@users.noreply.github.com>
2021-10-05 17:39:03 +0700

    Updated the print message to make it look fancy
    made it have "You get: "

--- first assignment.py
+++ first assignment.py
@@ -4,12 +4,12 @@ number_1=eval(input("First number: "))
 number_2=eval(input("Second number: "))
 
 if (operation == 1):
-    print(number_1 + number_2)
+    print("You get:", number_1 + number_2)
 elif(operation == 2):
-    print(number_1 - number_2)
+    print("You get:", number_1 - number_2)
 elif(operation == 3):
-    print(number_1 * number_2)
+    print("You get:",number_1 * number_2)
 elif(operation == 4):
-    print(number_1 / number_2)
+    print("You get:", number_1 / number_2)
 else:
     print ("error")
